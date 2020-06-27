ef01086f134c9f9486a6397d9ea25c46ce5e1548
Roselma <roselma.mendes@gmail.com>
2020-06-27 13:45:09 -0300

    adiciona .env-sample e coloca outras respostas para o bot

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ client.login(process.env.DISCORD_TOKEN);
 
 client.on('message', message => {
 	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
+	}else if (message.content === '!user-info') {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}else if (message.author.username === process.env.USER_1) {
+		message.channel.send('olar Judith');
+	}else if (message.author.username === process.env.USER_2) {
+		message.channel.send('Perdeu a referencia?');
 	}
 });
