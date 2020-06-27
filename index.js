1ff61acdf255ee70a37670b2e684b22636207bdf
Roselma <roselma.mendes@gmail.com>
2020-06-27 14:11:11 -0300

    conserta erros de lint

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ client.login(process.env.DISCORD_TOKEN);
 client.on('message', message => {
 	if (message.content === '!ping') {
 		message.channel.send('Pong.');
-	}else if (message.content === '!user-info') {
+	}
+	else if (message.content === '!user-info') {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}else if (message.author.username === process.env.USER_1) {
+	}
+	else if (message.author.username === process.env.USER_1) {
 		message.channel.send('olar Judith');
-	}else if (message.author.username === process.env.USER_2) {
+	}
+	else if (message.author.username === process.env.USER_2) {
 		message.channel.send('Perdeu a referencia?');
 	}
 });
